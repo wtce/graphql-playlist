6d3bb75eb77340e6e08fac3e40604c45b4b4f1b7
emily599 <emilydoan52@gmail.com>
2020-04-03 15:29:48 -0700

    Add graphiql to test queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,17 +1,28 @@
 const graphql = require('graphql');
+const _ =require('lodash');
 
-const {GraphqlObjectType, GraphQLString, GraphQLSchema} = graphql;
+//grab properties from graphql package
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
 
-const AnimeType = new GraphqlObjectType({
+//dummy data
+var animes = [
+    {name: 'Doctor Stone', genre: 'Sci-Fi', id: '1'},
+    {name: 'My Hero Academia', genre: 'Action', id: '2'},
+    {name: 'Fruits Basket', genre: 'Slice of life', id: '3'},
+];
+
+//define first object type which contain different fields
+const AnimeType = new GraphQLObjectType({
    name:'Anime',
    fields:() => ({
-       id: {type:GraphQLString},
+       id: {type: GraphQLString},
        name: {type: GraphQLString},
-       genre:{type:GraphQLString}
+       genre: {type: GraphQLString}
    })
 });
 
-const RootQuery = GraphqlObjectType({
+//define root query which is used to jump into the graph
+const RootQuery = new GraphQLObjectType({
    name:'RootQueryType',
    fields:{
        anime:{
@@ -19,11 +30,13 @@ const RootQuery = GraphqlObjectType({
            args:{id: {type: GraphQLString}},
            resolve(parent,args){
                //code to get data from db/other source
+                return _.find(animes, {id: args.id});
            }
        }
    }
 });
 
+//defining which query to use (i.e root query)
 module.exports = new GraphQLSchema({
     query: RootQuery
 });
