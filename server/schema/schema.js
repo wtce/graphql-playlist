9f9107fb2b14f7882de0da1a48c7552313088b96
emily599 <emilydoan52@gmail.com>
2020-04-06 14:19:37 -0700

    Add studio type object

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,23 +2,44 @@ const graphql = require('graphql');
 const _ =require('lodash');
 
 //grab properties from graphql package
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInt
+    } = graphql;
 
 //dummy data
 var animes = [
     {name: 'Doctor Stone', genre: 'Sci-Fi', id: '1'},
     {name: 'My Hero Academia', genre: 'Action', id: '2'},
-    {name: 'Fruits Basket', genre: 'Slice of life', id: '3'},
+    {name: 'Tokyo Ghoul', genre: 'Mystery', id: '3'},
+];
+
+var studios = [
+    {name: 'TMS Entertainment', year: '1964', id: '1'},
+    {name: 'Bones', year: '1998', id: '2'},
+    {name: 'Studio Pierrot', year: '1979', id: '3'},
 ];
 
 //define first object type which contain different fields
 const AnimeType = new GraphQLObjectType({
    name:'Anime',
    fields:() => ({
-       id: {type: GraphQLString},
+       id: {type: GraphQLID},
        name: {type: GraphQLString},
        genre: {type: GraphQLString}
    })
+});
+
+const StudioType = new GraphQLObjectType({
+    name:'Studio',
+    fields:() => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        year: {type: GraphQLInt}
+    })
 });
 
 //define root query which is used to jump into the graph
@@ -27,10 +48,17 @@ const RootQuery = new GraphQLObjectType({
    fields:{
        anime:{
            type:AnimeType,
-           args:{id: {type: GraphQLString}},
+           args:{id: {type: GraphQLID}},
            resolve(parent,args){
                //code to get data from db/other source
                 return _.find(animes, {id: args.id});
+           }
+       },
+       studio:{
+           type: StudioType,
+           args: {id: {type: GraphQLID}},
+            resolve(parent, args){
+               return _.find(studios, {id: args.id})
            }
        }
    }
